68a32d7fe2418131dca54cda910d814b05d59a6f
DarkZel2 <154449407+DarkZel2@users.noreply.github.com>
2024-02-26 17:20:35 -0500

    update

--- code/conf/db.js
+++ code/conf/db.js
@@ -1,1 +1,23 @@
 const mongoose = require("mongoose");
+const DB_URL = 'mongodb+srv://sartunduaga:0369@cluster0.avxkuvc.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0'
+
+module.exports = () => {
+    const connect = () => {
+        mongoose.connect(
+            DB_URL,
+            {
+                keepAlive: true,
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            },
+            (err) => {
+                if(err){
+                    console.log('DB connection error')
+                }else{
+                    console.log('Connection established') 
+                }
+            }
+        )
+    }
+    connect();
+}
--- code/server.js
+++ code/server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./app/routes/user')
+const Initdb = require('./conf/db')
 
 app.use(router)
 
 app.listen(port, () => {
     console.log('La pagina funciona')
 })
+
+Initdb();
