72d2f392ed13b0d16deb26e780fa1cfd9861a968
DarkZel2 <154449407+DarkZel2@users.noreply.github.com>
2024-02-26 16:16:17 -0500

    update

--- code/server.js
+++ code/server.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const router = require('./app/routes/user')
 
-app.get('/', (req, res) => {
-    res.send({
-        data: 'Hola'
-    })
-})
+app.use(router)
 
 app.listen(port, () => {
     console.log('La pagina funciona')
